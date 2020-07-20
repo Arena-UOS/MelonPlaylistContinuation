86970425ee9a67ed0ce95dac4570f7fcc0579ea2
Daehyun LabMSI <1phantasmas@korea.ac.kr>
2020-07-20 14:21:58 +0900

    [main.py] add argparser

--- main.py
+++ main.py
@@ -4,23 +4,50 @@ from neighbor import Neighbor
 from neighbor_knn import NeighborKNN
 from data_util import *
 from arena_util import load_json, write_json
-from title_to_Tag import Title_to_tag
+
+
+import argparse
+
+parser = argparse.ArgumentParser(description='User kNN')
+parser.add_argument('--khaiii', '-k', type=bool, default=True, help='khaiii availability')
+parser.add_argument('--train_path', '-t', type=str, default='res/train.json', help='path of train.json')
+parser.add_argument('--val_path', '-v', type=str, default='res/val.json', help='path of val.json')
+parser.add_argument('--song_path', '-s', type=str, default='res/song_meta.json', help='path of song_meta.json')
+parser.add_argument('--val_conv_path', '-vcp', type=str, default='res/val_title.json', help='if khaiii doesnt work-windows')
+parser.add_argument('--alpha', '-a', type=float, default=0.7, help='alpha')
+parser.add_argument('--beta', '-b', type=float, default=0.0, help='beta')
+parser.add_argument('--song_k', '-sk', type=int, default=100, help='how many song similarity to embrace')
+parser.add_argument('--tag_k', '-tk', type=int, default=100, help='how many tag similairty to embrace')
+parser.add_argument('--song_k_step', '-ss', type=int, default=10, help='how many steps to go further after song less than 100')
+parser.add_argument('--tag_k_step', '-ts', type=int, default=10, help='how many steps to go further after tag less than 10')
+parser.add_argument('--rho', '-r', type=float, default=0.4, help='rho')
+parser.add_argument('--weight_val_songs', '-wvs', type=float, default=0.9, help='weights to validation song compared to train_song')
+parser.add_argument('--weight_val_tags', '-wvt', type=float, default=0.7, help='weights to validation tag compared to train_tag')
+parser.add_argument('--sim_songs', '-sims', type=str, default='idf', help='methods to calculate similarity of songs')
+parser.add_argument('--sim_tags', '-simt', type=str, default='idf', help='methods to calculate similarity of tags')
+parser.add_argument('--sim_normalize', '-simn', type=bool, default=False, help='to normalizae simlarity or not')
+args = parser.parse_args()
 
 ### 1. data & preprocessing
 ### 1.1 load data
-train_path = "res/train.json"
-val_path   = "res/val.json"
+train_path = args.train_path
+val_path   = args.val_path
 
-song_meta = load_json("res/song_meta.json")
-train     = load_json("res/train.json")
-val       = load_json("res/val.json")
+song_meta = load_json(args.song_path)
+train     = load_json(train_path)
+val       = load_json(val_path)
 
 song_meta = pd.DataFrame(song_meta)
 train     = pd.DataFrame(train)
 val       = pd.DataFrame(val)
 
-### 1.2 only_title chage to tags
-val = Title_to_tag(train1=train_path, val1=val_path).change()
+if args.khaiii:
+    from title_to_Tag import Title_to_tag
+    ### 1.2 only_title chage to tags
+    val = Title_to_tag(train1=train_path, val1=val_path).change()
+
+else:
+    val = load_json(args.val_conv_path)
 ### 1.3 convert "tag" to "tag_id"
 tag_to_id, id_to_tag = tag_id_meta(train, val)
 train = convert_tag_to_id(train, tag_to_id)
@@ -29,8 +56,8 @@ val   = convert_tag_to_id(val  , tag_to_id)
 
 ### 2. modeling : Neighbor
 ### 2.1 hyperparameters: pow_alpha, pow_beta
-pow_alpha = 0.7
-pow_beta = 0.0
+pow_alpha = args.alpha
+pow_beta = args.beta
 
 ### 2.2 run Neighbor.predict() : returns pandas.DataFrame
 pred = Neighbor(pow_alpha=pow_alpha, pow_beta=pow_beta, \
@@ -48,18 +75,18 @@ pred = Neighbor(pow_alpha=pow_alpha, pow_beta=pow_beta, \
 ### 3. modeling : NeighborKNN
 ### 3.1 hyperparameters: k, rho, weights
 ### 3.2 parameters: sim_songs, sim_tags, sim_normalize
-song_k = 100
-tag_k  = 100
-song_k_step = 100
-tag_k_step  = 100
-rho = 0.4
-weight_val_songs  = 0.9
+song_k = args.song_k
+tag_k  = args.tag_k
+song_k_step = args.song_k_step
+tag_k_step  = args.tag_k_step
+rho = args.rho
+weight_val_songs  = args.weight_val_songs
 weight_pred_songs = 1 - weight_val_songs
-weight_val_tags   = 0.7
+weight_val_tags   = args.weight_val_tags
 weight_pred_tags  = 1 - weight_val_tags
-sim_songs = "idf"
-sim_tags  = "idf"
-sim_normalize = False
+sim_songs = args.sim_songs
+sim_tags  = args.sim_tags
+sim_normalize = args.sim_normalize
 
 ### 3.3 run NeighborKNN.predict() : returns pandas.DataFrame
 pred = NeighborKNN(song_k=song_k, tag_k=tag_k, rho=rho, \
@@ -83,3 +110,4 @@ write_json(pred, "results.json") # path???
 # fname2 = f"neighbor-knn{version}_k{k}rho{int(rho * 10)}s{int(weight_val_songs * 10)}t{int(weight_val_tags * 10)}_{sim_songs}{sim_tags}{sim_normalize}"
 # pred.to_json(f'{path}/{fname2}.json', orient='records')
 ### ======================================================================
+
